135670b28dc88f90883915fa962e6dec522cf9d2
abraralidev <abraraliiofficial@gmail.com>
2022-12-13 12:43:45 +0500

    class4

---
+++ class_4/lib/Screens/home.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              color: Colors.red,
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 100,
+            // padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.all(10),
+            color: Colors.red,
+          ),
+          const Text("Data",
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+
+// MediaQuery
+// padding
+// margin
+// 

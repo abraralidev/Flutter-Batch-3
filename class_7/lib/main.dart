cf20b06d34ba3bcd6de1d3ef99da58344c61e013
abraralidev <abraraliiofficial@gmail.com>
2022-12-26 17:22:20 +0500

    class7

--- class_7/lib/main.dart
+++ class_7/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:class_7/screens/home.dart';
 import 'package:flutter/material.dart';
+
+import 'Screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-home: const Home(),
+      home: const HomeScreen(),
     );
   }
 }
--- class_7/lib/screens/home.dart
+++ class_7/lib/screens/home.dart
@@ -1,124 +1,204 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI Calculator'),
-        centerTitle: true,
+        backgroundColor: Colors.black87,
+        appBar: AppBar(
+          title: const Text('Bmi Calculator'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            // ===============================Row Number 1==================================================
+            Expanded(
+                child: Row(
+              children: [
+                ReusedCard(
+                  cardchild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.male,
+                          size: 90,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Male',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ))
+                      ]),
+                ),
+                ReusedCard(
+                  cardchild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.female,
+                          size: 90,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Female',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ))
+                      ]),
+                ),
+              ],
+            )),
+            // ===============================Row Number 2==================================================
+            Expanded(
+                child: Row(
+              children: [
+                ReusedCard(
+                  cardchild: Column(),
+                )
+              ],
+            )),
+            // ===============================Row Number 3==================================================
+            Expanded(
+                child: Row(
+              children: [
+                ReusedCard(
+                  cardchild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'WEIGHT',
+                          style: TextStyle(
+                            fontSize: 20,
+                            // fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const Text(
+                          '60',
+                          style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FloatingActionButton(
+                              onPressed: () {},
+                              backgroundColor: Colors.redAccent,
+                              child: const Icon(
+                                Icons.remove,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            FloatingActionButton(
+                              onPressed: () {},
+                              backgroundColor: Colors.redAccent,
+                              child: const Icon(
+                                Icons.add,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        )
+                      ]),
+                ),
+                ReusedCard(
+                  cardchild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'AGE',
+                          style: TextStyle(
+                            fontSize: 20,
+                            // fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const Text(
+                          '60',
+                          style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FloatingActionButton(
+                              onPressed: () {},
+                              backgroundColor: Colors.redAccent,
+                              child: const Icon(
+                                Icons.remove,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            FloatingActionButton(
+                              onPressed: () {},
+                              backgroundColor: Colors.redAccent,
+                              child: const Icon(
+                                Icons.add,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        )
+                      ]),
+                ),
+              ],
+            )),
+          ],
+        ));
+  }
+}
+
+class ReusedCard extends StatelessWidget {
+  const ReusedCard({
+    Key? key,
+    required this.cardchild,
+  }) : super(key: key);
+
+  final Widget cardchild;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.red.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: const Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: cardchild,
       ),
-      body: Column(children: [
-//----------------------------Number 1-------------------------------------
-        Expanded(
-            child: Row(
-          children: [
-            Expanded(
-              //-----------------Male Card----------------------
-              child: Container(
-                color: Colors.blue,
-                margin: const EdgeInsets.all(12),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(
-                      Icons.male,
-                      size: 80,
-                      color: Colors.white,
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Text(
-                      "Male",
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              //-----------------Female Card----------------------
-              child: Container(
-                color: Colors.blue,
-                margin: const EdgeInsets.all(12),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(
-                      Icons.female,
-                      size: 80,
-                      color: Colors.white,
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Text(
-                      "Female",
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ],
-        )),
-        //----------------------------Number 2-------------------------------------
-        Expanded(
-            child: Row(
-          children: [
-            Expanded(
-              child: Container(
-                color: Colors.blue,
-                margin: const EdgeInsets.all(12),
-              ),
-            ),
-          ],
-        )),
-        //----------------------------Number 3-------------------------------------
-        Expanded(
-            child: Row(
-          children: [
-            Expanded(
-              child: Container(
-                color: Colors.blue,
-                margin: const EdgeInsets.all(12),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.blue,
-                margin: const EdgeInsets.all(12),
-                child: Image.asset(
-                  'assets/image1.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ],
-        )),
-//-----------------------------Button--------------------------------------
-        Container(
-          width: double.infinity,
-          height: 60,
-          color: Colors.blue,
-          child: const Center(
-            child: Text(
-              "Calculate",
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-          ),
-        )
-      ]),
     );
   }
 }
 
 // Expanded widget
-
-
-// Terminal Commands
-// flutter clean
-// flutter pub get
